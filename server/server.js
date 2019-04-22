bd46bf4664691bbcc871eaf966f6f007fe3ae653
drzz <skyofsmith@163.com>
2019-04-22 22:42:07 +0800

    add server part

---
+++ server/server.js
@@ -0,0 +1,5 @@
+const app = require("express")();
+
+app.listen(8079, () => {
+  console.log("server start up!");
+});
---
+++ server/utils/hash.js
@@ -0,0 +1,10 @@
+const crypto = require("crypto");
+function hashFactory(type) {
+  return function(str) {
+    let hash = crypto.createHash(type);
+    hash.update(str);
+    return hash.digest("hex");
+  };
+}
+exports.md5 = hashFactory("md5");
+exports.sha256 = hashFactory("sha256");
