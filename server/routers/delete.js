cb2c815ed09cf2c702d503fa3beb916f19993ee0
drzz <skyofsmith@163.com>
2019-04-27 22:40:32 +0800

    modify delete

---
+++ server/routers/delete.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const mysql = require("../utils/mysql");
+const { md5, sha256 } = require("../utils/hash");
+
+const router = express.Router();
+
+router.post("/delete", (req, res) => {
+  console.log("/delete", req.body);
+  let { username, password } = req.body;
+  if (!username || !password) {
+    res.json({
+      status: "failed",
+      message: "username or password can not be empty"
+    });
+    return;
+  }
+  let password_md5 = md5(password);
+  let password_sha256 = sha256(password);
+  mysql.query(
+    "select * from user where username = ? and password_md5 = ? and password_sha256 = ?",
+    [username, password_md5, password_sha256],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.json({
+          status: "failed",
+          message: "username or password not correct"
+        });
+        return;
+      }
+      if (!result || !result.length) {
+        res.json({
+          status: "failed",
+          message: "username or password not correct"
+        });
+        return;
+      }
+      mysql.query(
+        "delete from user where username = ? and password_md5 = ? and password_sha256 = ?",
+        [username, password_md5, password_sha256],
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            res.json({
+              status: "failed",
+              message: "username or password not correct"
+            });
+            return;
+          }
+          if (!result) {
+            res.json({
+              status: "failed",
+              message: "delete failed"
+            });
+            return;
+          }
+          res.json({
+            status: "success",
+            message: "delete success"
+          });
+        }
+      );
+    }
+  );
+});
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const CROS = require("./utils/cros");
 const register = require("./routers/register");
+const deleteUser = require("./routers/delete");
 
 app.use(express.json());
 app.use(CROS);
 app.use("/user", register);
+app.use("/user", deleteUser);
 
 app.listen(8079, () => {
   console.log("server start up!");
