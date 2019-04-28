946c17bbf1dadf3ba3daeee5c7fb71e7b7757550
drzz <skyofsmith@163.com>
2019-04-28 23:45:18 +0800

    add modifyInfo

---
+++ server/routers/modifyInfo.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const mysql = require("../utils/mysql");
+const { md5, sha256 } = require("../utils/hash");
+
+const router = express.Router();
+
+router.post("/modifyInfo", (req, res) => {
+  console.log("post: /register", req.body);
+  // mysql.connect();
+  let { username, password, email, address, gender, realName } = req.body;
+  if (!username || !password) {
+    res.json({
+      status: "failed",
+      message: "username or password can not be empty"
+    });
+  }
+  let password_md5 = md5(password);
+  let password_sha256 = sha256(password);
+  mysql.query(
+    "select * from user where username = ?",
+    [username],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (!result) {
+        console.log("error, no result");
+        return;
+      }
+      if (result.length) {
+        console.log("error, user already exist");
+        res.json({
+          status: "failed",
+          message: "user already exist"
+        });
+        return;
+      }
+      mysql.query(
+        "insert into user set username = ?, password_md5 = ?, password_sha256 = ?, email = ?, address = ?,gender = ?, real_name = ?",
+        [
+          username,
+          password_md5,
+          password_sha256,
+          email,
+          address,
+          gender,
+          realName
+        ],
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            res.json({
+              status: "failed",
+              message: ""
+            });
+            return;
+          }
+          if (result) {
+            console.log(result);
+          }
+          res.json({
+            status: "success"
+          });
+        }
+      );
+    }
+  );
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const CROS = require("./utils/cros");
 const register = require("./routers/register");
+const modifyInfo = require("./routers/modifyInfo");
 const deleteUser = require("./routers/delete");
 
 app.use(express.json());
 app.use(CROS);
 app.use("/user", register);
+app.use("/user", modifyInfo);
 app.use("/user", deleteUser);
 
 app.listen(8079, () => {
